433494ccc56b53dbd9c00021d18f158c3bbcfccb
Diego <info@diego-leiva.com>
2018-11-21 18:17:25 -0300

    Agregar archivos de proyecto.

---
+++ Interfaces/IOperadorRepository.cs
@@ -0,0 +1,16 @@
+﻿using DLL;
+using System.Collections.Generic;
+
+namespace Interfaces
+{
+    public interface IOperadorRepository
+    {
+        Operador LoginOperador(string usuario, string password);
+        bool AltaOperador(Operador operador);
+        List<OperadorLista> GetOperadores();
+        Operador GetOperadorById(int id);
+        bool ModificarOperador(Operador operador);
+        List<OperadorLista> GetByName(string nombre);
+        List<OperadorLista> GetByDocument(string documento);
+    }
+}
